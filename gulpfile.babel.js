1df1bb2616f6373de48564ed928f1f937f2de65c
Dheeraj Khushalani <mailtodhirajk@gmail.com>
2016-11-04 19:19:43 +1100

    Initial Commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,27 @@
+import gulp from 'gulp';
+import selenium from 'selenium-standalone';
+import webdriver from 'gulp-webdriver';
+
+let seleniumServer;
+
+gulp.task('selenium', (done) => {
+  selenium.install({logger: console.log}, () => {
+    selenium.start((err, child) => {
+      if (err) { return done(err); }
+      seleniumServer = child;
+      done();
+    });
+  });
+});
+
+gulp.task('e2e', ['selenium'], () => {
+  return gulp.src('wdio.conf.js')
+    .pipe(webdriver()).on('error', () => {
+      seleniumServer.kill();
+      process.exit(1);
+    });
+});
+
+gulp.task('test', ['e2e'], () => {
+  seleniumServer.kill();
+});
